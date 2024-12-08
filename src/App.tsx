d1f61567fb1d1a5856cdafeb8d134f73a050aea9
brait <lorimiervb@gmail.com>
2024-12-07 21:38:53 -0300

    alterado para abrir a camera

--- src/App.tsx
+++ src/App.tsx
@@ -3,23 +3,52 @@ import { listenToData } from './firebaseConfig'
 
 export default function App() {
   const [showCamera, setShowCamera] = useState(false)
-  const videoRef = useRef(null)
+  const videoRef = useRef<HTMLVideoElement>(null)
 
   useEffect(() => {
     listenToData('/Sensor/movimento', (cameraState) => {
       if (cameraState !== null) {
         setShowCamera(cameraState)
 
+        if (cameraState) {
+          startCamera()
+        } else {
+          stopCamera()
+        }
+
         setTimeout(() => {
           setShowCamera(false)
-      }, 5000)
-    }
+        }, 5000)
+      }
     })
 
     return () => {
       console.log("Cleanup: Parando de escutar dados do Firebase")
+      stopCamera()  // Cleanup: parar a câmera se necessário
     }
   }, [])
+
+  // Função para iniciar a câmera
+  const startCamera = async () => {
+    try {
+      const stream = await navigator.mediaDevices.getUserMedia({ video: {facingMode: "user"} })
+      if (videoRef.current) {
+        videoRef.current.srcObject = stream
+      }
+    } catch (error) {
+      console.error("Erro ao acessar a câmera:", error)
+    }
+  }
+
+  // Função para parar a câmera
+  const stopCamera = () => {
+    const stream = videoRef.current?.srcObject as MediaStream
+    const tracks = stream?.getTracks()
+    tracks?.forEach(track => track.stop())
+    if (videoRef.current) {
+      videoRef.current.srcObject = null
+    }
+  }
 
   return (
     <div
@@ -34,21 +63,19 @@ export default function App() {
       }}
     >
       {showCamera ? (
-        <video ref={videoRef} style={{ width: '100%', height: '100%' }} />
+        <video ref={videoRef} style={{ width: '100%', height: '100%' }} autoPlay />
       ) : (
-        <>
-          <div
-            style={{
-              width: '50px',
-              height: '50px',
-              border: '6px solid #ffffff', // Cor do círculo
-              borderTop: '6px solid #0d2a3d', // Cor do loader
-              borderRadius: '50%',
-              marginTop: '20px',
-              animation: 'spin 1s linear infinite', // Animação de rotação
-            }}
-          />
-        </>
+        <div
+          style={{
+            width: '50px',
+            height: '50px',
+            border: '6px solid #ffffff', // Cor do círculo
+            borderTop: '6px solid #0d2a3d', // Cor do loader
+            borderRadius: '50%',
+            marginTop: '20px',
+            animation: 'spin 1s linear infinite', // Animação de rotação
+          }}
+        />
       )}
     </div>
   )
